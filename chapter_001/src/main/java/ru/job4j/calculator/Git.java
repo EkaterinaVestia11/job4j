4c9a82c66e7341ecb3e5bfdf30dda3600cf7456e
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2019-08-08 17:43:56 +0300

    5. Интеграция Git и IDEA[#163735]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Git.java
@@ -0,0 +1,8 @@
+package ru.job4j.calculator;
+
+public class Git{
+    public static void add(double first, double second) {
+        double result =  first + second;
+        System.out.println(first + "+" + second + " = " + result);
+    }
+}
