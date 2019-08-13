f93a48688e6adb24a86303a486da2d267aedb1c9
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2019-08-13 19:53:20 +0300

    5.2. Создать программу, вычисляющую факториал.[#163674]

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -5,7 +5,7 @@ public class Counter{
         int sum=0;
         int a;
         for(a=start; a <= finish; a++) {
-            if (start % 2 == 0)
+            if (a % 2 == 0)
             sum+=a; // 0 + 2 + 4 + 6 + 8 + 10
         }
         return sum;
--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -12,9 +12,9 @@ public class CounterTest{
         //напишите здесь тест, проверяющий, что сумма чётных чисел от 1 до 10 при вызове метода counter.add будет равна 30.
         int start = 1;
         int finish = 10;
-       // int expected = 30;
+        int expected = 30;
         int out = Counter.add(start, finish);//Counter add = new Counter();
-           Assert.assertEquals(30,30, 0.1);
+           Assert.assertEquals(expected, out);
         //Assert.assertEquals ( expected ,  out  );//assertThat  ( out, is ( 30 ) );
     }
       @Test
@@ -22,17 +22,17 @@ public class CounterTest{
         //напишите здесь тест, проверяющий, что сумма чётных чисел от 11 до 15 при вызове метода counter.add будет равна 30.
         int start = 11;
         int finish = 15;
-       // int expected = 26;
+        int expected = 26;
         int out = Counter.add(start, finish);//Counter add = new Counter();
-        Assert.assertEquals(26, 26, 0.1);
+          Assert.assertEquals(expected, out);
     }
     @Test
-    public void whenSumEvenNumbersFromOneToTenThenThirty2() {
+    public void whenSumEvenNumbersFromOneToTenThenThirty2(){
         //напишите здесь тест, проверяющий, что сумма чётных чисел от 35 до 40 при вызове метода counter.add будет равна 30.
-        int start = 35;
-        int finish = 40;
-        // int expected = 26;
-        int out = Counter.add(start, finish);//Counter add = new Counter();
-        Assert.assertEquals(74, 74, 0.1);
+        int start=35;
+        int finish=40;
+        int expected=114;
+        int out=Counter.add(start,finish);//Counter add = new Counter();
+        Assert.assertEquals(expected,out);
     }
 }
