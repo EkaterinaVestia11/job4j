a4e57b716a7ce889fbe07d4933dc145fa8e49e69
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2019-09-13 14:23:56 +0300

    Factorial

--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -2,9 +2,6 @@ package ru.job4j.loop;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 public class FactorialTest {
     @Test
     public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
