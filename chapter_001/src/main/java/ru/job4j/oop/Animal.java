ecc36f8064903853802f11638947340045af3ea3
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2019-09-30 16:06:51 +0300

    Перегрузка конструктора. [#173890]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Animal.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+public class Animal {
+    private String name;
+
+    public Animal() {
+        System.out.println("Animal");
+    }
+
+    public Animal(String name) {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/oop/Tiger.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Tiger extends Predator {
+    public Tiger() {
+        System.out.println("Tiger");
+    }
+
+    public Tiger(String name) {
+        super(name);
+    }
+
+    public static void main(String[] args) {
+         Tiger tiger = new Tiger();
+    }
+}
