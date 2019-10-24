94dcfc5926787f2287688a8208dc5b3c1bac5554
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2019-10-24 16:49:05 +0300

    Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения[#163780]

--- chapter_02/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_02/src/main/java/ru/job4j/tracker/StartUI.java
@@ -15,8 +15,40 @@ public class StartUI {
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
-//            } else if (...) {
-//             Добавить остальные действия системы по меню.
+            } else if (select == 1) {
+                System.out.println("=== Get a list of items ====");
+                System.out.print("Select name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.findAll();
+            } else if (select == 2) {
+                System.out.println("=== Change item ====");
+                System.out.print("Enter name and id: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                String id = scanner.nextLine();
+                tracker.replace(id, item);
+            } else if (select == 3) {
+                System.out.println("=== Delete item ====");
+                System.out.print("Enter name and id: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                String id = scanner.nextLine();
+                tracker.delete(id);
+            } else if (select == 4) {
+                System.out.println("=== Search by id ====");
+                System.out.print("Enter id: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                String id = scanner.nextLine();
+                tracker.findById(id);
+            } else if (select == 5) {
+                System.out.println("=== Search by key ====");
+                System.out.print("Enter key: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                String key = scanner.nextLine();
+                tracker.findByName(key);
             } else if (select == 6) {
                 run = false;
             }
@@ -25,9 +57,14 @@ public class StartUI {
 
     private void showMenu() {
         System.out.println("Menu.");
-        // добавить остальные пункты меню.
+        System.out.println("0. Add new Item");
+        System.out.println("1. Show all items");
+        System.out.println("2. Edit item");
+        System.out.println("3. Delete item");
+        System.out.println("4. Find item by Id");
+        System.out.println("5. Find items by name");
+        System.out.println("6. Exit Program");
     }
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
