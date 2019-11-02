b0d17bce050876ef57bd70862ac6e7146c95c2ff
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2019-11-02 21:11:08 +0300

    [#16380]

--- chapter_02/src/main/java/ru/job4j/io/MagicBall.java
+++ chapter_02/src/main/java/ru/job4j/io/MagicBall.java
@@ -7,16 +7,7 @@ public class MagicBall {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
-        String questionFirst = input.nextLine();
-        System.out.println(questionFirst + " Слишком заумно:), введите, пожалуйста, закрытый вопрос)");
-        String questionSecond = input.nextLine();
-        System.out.println(" Выберите шары под номерами: ");
-        System.out.println(" 0 ");
-        System.out.println(" 1 ");
-        System.out.println(" 2 ");
-        System.out.println(" 3");
-        System.out.println(" Ваш выбор: ");
-        String questionThird = input.nextLine();
+        input.nextLine();
         int answer = new Random().nextInt(3);
         System.out.println(" Ответ на ваш вопрос: ");
         if ( answer == 1 ) {
--- chapter_02/src/main/java/ru/job4j/io/Matches.java
+++ chapter_02/src/main/java/ru/job4j/io/Matches.java
@@ -7,40 +7,17 @@ public class Matches{
         Scanner input=new Scanner(System.in);
         System.out.println("Поиграем? У нас 11 спичек,"+
                 " за один раз можно взять от 1 до 3 спичек, прсмотрим кто выиграет)");
-        boolean run=true;
-        while (run) {
-            System.out.print("Делаем ход: \n");
-            System.out.println("1 спичка");
-            System.out.println("2 спички");
-            System.out.println("3 спички");
-            System.out.print("Делайте ваш выбор: ");
-            int select=Integer.valueOf(input.nextLine());
-            int n = 11; //общее количество спичек
-            int m = 3; //максимальное взятие за ход
-            int x = 0; //взятие текущее
-            int[] array = new int[3];
-
-            if( n - m >= 0) {
-                x = m; //
-            } else { x = m - n;
-            }
-            for(int i = 0; i <= m; i++) {
-                if (i >= 0 || i <= 0) {
-                    System.out.print("Напоминание) от 1 до 3 ");
-                    System.out.print("Повторите выбор: ");
-                }else { int y = n - x; //остаток после взятия пользователем
-                }
-            }
-            if ( select == 0 )  {
-                if (x > 0 ) {
-                   continue;
-                } else if ( x == 0) {
-                    System.out.print("Подравления с победой))) вы- победитель!!! ");
-                } else select++;
-            }
+        boolean turn=true;
+        short matches=11; //общее количество спичек
+        while (matches > 0) {
+            System.out.print(turn ? "first" : "second");
+            int size=Integer.parseInt(input.nextLine());
+            //
+            turn=!turn;
         }
     }
 }
+
 
 
 
