203843f9e380a10800f09f5d862a2eec2a7a268b
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2019-12-21 21:25:21 +0300

    if-else-throw[#163786]

--- chapter_02/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_02/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -1,28 +1,33 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Vector;
 
-public class ConsoleInput implements Input{
-    private Scanner scanner = new Scanner(System.in);
+public class ConsoleInput<question> implements Input{
+    private Scanner scanner=new Scanner (System.in);
+    private Vector validates;
+    private Arrays Arrais;
 
     @Override
     public String askStr(String question) {
-        System.out.print(question);
-        return scanner.nextLine();
+        System.out.print (question);
+        return scanner.nextLine ();
     }
 
     @Override
-    public int askInt( String question) {
-        return Integer.valueOf(askStr(question));
+    public int askInt(String question) {
+        return Integer.valueOf (askStr (question));
     }
 
     @Override
     public int askInt(String question,int max) {
-        int select = askInt(question);
-        if (select >= 0 && select < max) {
-            return select;
-        } else {
-            throw new IllegalStateException(String.format("Out of about %s > [0, %s]", select, max));
-        }
+        int select=askInt (question);
+           if ( select >= 0 && select < max ){
+              throw new IllegalStateException (String.format ("Out of about %s > [0, %s]",select,max));
+                    }
+          return select;
     }
+
 }
+
