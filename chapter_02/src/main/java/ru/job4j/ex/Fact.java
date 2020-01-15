5e5fd042d0d945b2c0c33507da80d2a5b4c5233b
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-01-15 17:28:37 +0300

    [#219367]

--- chapter_02/src/main/java/ru/job4j/ex/Fact.java
+++ chapter_02/src/main/java/ru/job4j/ex/Fact.java
@@ -1,4 +1,19 @@
 package ru.job4j.ex;
 
-public class Fact{
+public class Fact {
+
+    public static void main(String[] args) {
+        System.out.println("Factorial es :" + calc(0));
+        }
+
+        public static int calc(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("Start should be less then finish.");
+        }
+         int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
 }
