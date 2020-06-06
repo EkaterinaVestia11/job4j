d2dcf606f08dadf30a55e358794f1c20b4d225fd
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-06-06 14:44:58 +0300

    JavaDoc. Code convention [#163676]

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -1,18 +1,52 @@
 package ru.job4j.converter;
 
+/**
+ * Class Класс для конвертации валюты
+ * @author Екатерина
+ * @since 06.06.2020
+ * @version 1
+ */
 public class Converter {
+    /**
+     * Метод для конвертация рублей в евро
+     * @param value ruble
+     * @return euro
+     */
     public static int rubleToEuro(int value) {
         return value / 70;
     }
+
+    /**
+     * Метод для конвертация рублей в доллары
+     * @param value ruble
+     * @return dollar
+     */
     public static int rubleToDollar(int value) {
         return value / 60;
     }
+
+    /**
+     * Метод для конвертация евро в рубли
+     * @param value euro
+     * @return ruble
+     */
     public static int euroToRuble(int value) {
         return value * 70;
     }
+
+    /**
+     * Метод для конвертация долларов в рубли
+     * @param value  dollar
+     * @return ruble
+     */
     public static int dollarToRuble(int value) {
         return value * 60;
     }
+
+    /**
+     * Main. Вывод на консоль вариантов обмена
+     * @param args-args
+     */
     public static void main(String[] args) {
         int euro = rubleToEuro(140);
         int dollar = rubleToDollar(120);
--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber1.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber1.java
@@ -4,11 +4,12 @@ public class PrimeNumber1 {
     public int calc( int finish ) {
         int count  = 0;
         for(int number = 2; number < finish; number++) {
-           for(int i = 2; i <= number; i++)
+         // for(int i = 2; i <= number; i++)
             if (CheckPrimeNumber.check(number)){
-               if (i % number == 0) {
-                   count++;
-               }
+                int i = 2;
+                if (i % number == 0){
+                    count++;
+                }
             }
         }
         return count;
