8ebd904c7f79edfa4147770432d4ed5ad13c5f16
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-06-21 20:21:05 +0300

    [# 163757]

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber1.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber1.java
@@ -2,7 +2,7 @@ package ru.job4j.loop;
 
 public class PrimeNumber1 {
     public int calc(int finish) {
-        int count=0;
+        int count = 0;
         for(int number=2; number < finish; number++) {
             if (CheckPrimeNumber.check(number)) {
                 count++;
