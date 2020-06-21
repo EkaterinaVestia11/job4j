b4e3476833ac96d125cc55850474faba8e3ebbbe
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-06-21 21:14:49 +0300

    Простые числа. [#163679]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -6,8 +6,8 @@ public class CheckPrimeNumber {
            for (int i = 2; i < finish; i++) {
                 if (finish  %  i == 0) {
                     prime = false;
+                    break;
                 }
-                break;
             }
         return prime;
     }
